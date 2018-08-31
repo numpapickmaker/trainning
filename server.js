18f4f715bfff7d112a71b3b3a23fa12fd501773d
numpapickmaker <numpapickmaker@users.noreply.github.com>
2018-08-31 22:50:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,9 @@ var http = require('http');
 var fileSystem = require('fs');
 http.createServer(function (req, res) {
    // write the code here if it needs to execute every time
-   //res.writeHead(200, {'Content-Type': 'text/plain'});
-   //res.end("this is a test page");
-   fileSystem.readFile('./index.html', function(error, fileContent){
-		if(error){
-			resp.writeHead(500, {'Content-Type': 'text/plain'});
-			resp.end('Error');
-		}
-		else{
-			resp.writeHead(200, {'Content-Type': 'text/html'});
-			resp.write(fileContent);
-			resp.end();
-		}
-	});
-
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   res.end("this is a test page");
+   
  }).listen(process.env.PORT || 8080,() =>console.log('ok')); 
 
 
